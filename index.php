f3433146a8dd6ddd371ec9a01ce7620c0f867004
Dyrehauge <dthomasn@gmail.com>
2015-03-31 17:33:47 +0200

    Push Style

---
+++ index.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css">
+	<title>Product-Idea</title>
+</head>
+<body>
+	<div class="navpusher"></div>
+	<nav>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Brands</a></li>
+			<li><a href="">Designers</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+		<ul>
+			<li><a href="">tellmewhatuwant</a></li>
+		</ul>
+		<ul>
+			<li><a href="">Favorites</a></li>
+			<li><a href="">Search</a></li>
+		</ul>
+	</nav>
+
+	</nav>
+	<div id="sidebar">
+
+<div class="checklist">
+	<h3>CATEGORIES</h3>
+	<ul>
+		<form action="">
+			<li><input type="checkbox" value="c1"><label for="c1"><span></span>New Arrivals</label></li>
+			<li><input type="checkbox" value="c2"><label for="c2"><span></span>Accesories</label></li>
+			<li><input type="checkbox" value="c3"><label for="c3"><span></span>Bags</label></li>
+			<li><input type="checkbox" value="c4"><label for="c4"><span></span>Dressed</label></li>
+			<li><input type="checkbox" value="c5"><label for="c5"><span></span>Jackets</label></li>
+			<li><input type="checkbox" value="c6"><label for="c6"><span></span>Jewelry</label></li>
+			<li><input type="checkbox" value="c7"><label for="c7"><span></span>Shoes</label></li>
+			<li><input type="checkbox" value="c8"><label for="c8"><span></span>Shirts</label></li>
+			<li><input type="checkbox" value="c9"><label for="c9"><span></span>Sweaters</label></li>
+			<li><input type="checkbox" value="c10"><label for="c10"><span></span>T-shirts</label></li>
+		</form>
+	</ul>
+</div>
+<div class="checklist doublerow">
+	<h3>Colors</h3>
+	<ul>
+		<li><a href="#"><span class="colors grey"></span><p class="color">Grey</p></a></li>
+		<li><a href="#"><span class="colors black"></span><p class="color">Black</p></a></li>
+		<li><a href="#"><span class="colors beige"></span><p class="color">Beige</p></a></li>
+		<li><a href="#"><span class="colors orange"></span><p class="color">Orange</p></a></li>
+		<li><a href="#"><span class="colors blue"></span><p class="color">Blue</p></a></li>
+		<li><a href="#"><span class="colors purple"></span><p class="color">Purple</p></a></li>
+		<li><a href="#"><span class="colors brown"></span><p class="color">Brown</p></a></li>
+		<li><a href="#"><span class="colors white"></span><p class="color">White</p></a></li>
+		<li><a href="#"><span class="colors red"></span><p class="color">Red</p></a></li>
+		<li><a href="#"><span class="colors green"></span><p class="color">Green</p></a></li>
+	</ul>
+</div>
+
+<div class="checklist doublerow">
+	<h3>SIZES</h3>
+	<ul>
+		<li><input type="checkbox"><a class="size" href="#">XS</a></li>
+		<li><input type="checkbox"><a class="size" href="#">S</a></li>
+		<li><input type="checkbox"><a class="size" href="#">M</a></li>
+		<li><input type="checkbox"><a class="size" href="#">L</a></li>
+		<li><input type="checkbox"><a class="size" href="#">XL</a></li>
+		<li><input type="checkbox"><a class="size" href="#">XXL</a></li>
+	</ul>
+</div>
+
+<div class="checklist">
+	<h3>CATEGORIES</h3>
+	<ul>
+		formsize
+	</ul>
+</div>
+	</div>
+	<div id="productwrap">
+		<div class="product-center">
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="2.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="3.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="2.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="3.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="2.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="2.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="3.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="2.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="3.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="product">
+				<div class="cover">
+					<img src="1.jpg" alt="">
+					<button>VIEW DETAILS</button>
+				</div>
+				<div class="product_con">
+					<h1>Addidas DMC</h1>
+					<span class="price">$39</span>
+					<p>Addidas Originals present DMC</p>
+					
+					<h2>Sizes</h2>
+					<span class="sizes">XS, S, M, L, XL, XXL</span>
+
+					<div class="color">
+						<h2>Colors</h2>
+						<span class="colors red"></span>
+						<span class="colors blue"></span>
+						<span class="colors white"></span>
+						<span class="colors green"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+	</div>
+</body>
+</html>
